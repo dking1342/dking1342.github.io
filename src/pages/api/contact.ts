8c31d400ef2296a8c233ca744db0d63366b75e6f
Kavooce <kavooce.cheetah@gmail.com>
2023-05-28 22:39:46 -0500

    nodemailer troubleshooting

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -18,7 +18,6 @@ let outputModel: OutputModel = {
 };
 const email = process.env.NEXT_PUBLIC_EMAIL;
 const pass = process.env.NEXT_PUBLIC_EMAIL_PASSWORD;
-console.log({ email, pass });
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   if (req.method === 'POST') {
