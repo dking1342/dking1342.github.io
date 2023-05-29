8144221790f071ad69d551efe32ebf681dcbcdb9
Kavooce <kavooce.cheetah@gmail.com>
2023-05-28 22:48:14 -0500

    added head to contact thanks and error pages

--- src/pages/contact-error.tsx
+++ src/pages/contact-error.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import globalStyles from '@/styles/Global.module.css';
 import MissionTexts from '@/components/MissionTexts';
+import Head from 'next/head';
 
 type Props = {};
 
 const contactError = (props: Props) => {
   return (
-    <section className={globalStyles.missionSection}>
-      <MissionTexts
-        color="primary"
-        text="There was an error when sending your information. Please try again or email us at our email address in the footer, or give us a call"
-        targets={[4, 6, 8, 11, 13, 21, 26]}
-      />
+    <section>
+      <Head>
+        <title>Error</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="mission compassion unsuccessful form submission page"
+        />
+      </Head>
+      <main>
+        <section className={globalStyles.missionSection}>
+          <MissionTexts
+            color="primary"
+            text="There was an error when sending your information. Please try again or email us at our email address in the footer, or give us a call"
+            targets={[4, 6, 8, 11, 13, 21, 26]}
+          />
+        </section>
+      </main>
     </section>
   );
 };
--- src/pages/thanks.tsx
+++ src/pages/thanks.tsx
@@ -1,17 +1,30 @@
 import MissionTexts from '@/components/MissionTexts';
 import React from 'react';
 import globalStyles from '@/styles/Global.module.css';
+import Head from 'next/head';
 
 type Props = {};
 
 const thanks = (props: Props) => {
   return (
-    <section className={globalStyles.missionSection}>
-      <MissionTexts
-        color="primary"
-        text="Thank you for getting in touch with us. We value your input and will reply back soon"
-        targets={[1, 2, 10, 12, 15]}
-      />
+    <section>
+      <Head>
+        <title>Thanks!</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="mission compassion successful form submission page"
+        />
+      </Head>
+      <main>
+        <section className={globalStyles.missionSection}>
+          <MissionTexts
+            color="primary"
+            text="Thank you for getting in touch with us. We value your input and will reply back soon"
+            targets={[1, 2, 10, 12, 15]}
+          />
+        </section>
+      </main>
     </section>
   );
 };
